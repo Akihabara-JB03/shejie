b94a4d2982bcf28fb1ae2ce76cca992525f0ca82
Akihabara-JB03 <akabanestation1007@gmail.com>
2026-09-02 23:05:53 +0900

    Update lexer.rs

--- src/lexer.rs
+++ src/lexer.rs
@@ -6,14 +6,24 @@ enum Token {
   IDENT(String),
   INT(i64),
   FLOAT(f64),
-  STRLITERAL(String)
+  STRLITERAL(String),
+  SPACE,
+  COMMENT(String),
+  NEWLINE,
+  PLUS,
+  MINUS,
+  DIV,
+  TIMES,
+  lLapen,
+  rLapen,
 }
 pub struct Lexer {
   input: Vec<char>, 
   position: usize,
   ch: char,
+  strmode: bool,
 }
-impl lexer() {
+impl Lexer {
   pub fn new(inputstr: &str) -> Self {
     let inpvec: Vec<char> = inputstr.chars().collect();
     let first_ch = if !inpvec.is_empty() { inpvec[0] } else { '\0' };
@@ -22,9 +32,16 @@ impl lexer() {
       input: inpvec,
       position: 0,
       ch: first_ch,
+      strmode: false,
     }
   }
   pub fn next_token(&mut self) -> Token {
-
+    match self.ch {
+      ' ' => {
+        if self.strmode {
+          
+        }
+      }
+    }
   }
 }
